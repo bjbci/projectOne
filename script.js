58879439de19862be8d071e402bc7a7de1292d5d
Brian Bulowicz <bajebi.bb@gmail.com>
2022-10-05 18:49:19 -0500

    second commit

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("whats up")
+
+
+
+
+{
+    "day": "25",
+    "month": "12",
+    "year": "1988",
+    "hour": "2",
+    "min": "30",
+    "name": "demo",
+    "lat": "25.123",
+    "lon": "82.34",
+    "tzone": "5.5"
+  }
