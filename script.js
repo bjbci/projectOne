624f7cbb0a3d8820e5ee9302327746f0f8a8f795
Hunter-Mayer <hmayer95@gmail.com>
2022-10-06 20:55:52 -0500

    added api_fetching to daily horoscope

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
-console.log("whats up")
+var form = document.querySelector('.signForm')
+var dateInput = document.getElementById('dateInput')
+var date = dateInput.value
+var signBtn = document.getElementById('signSubmit')
+var sign = document.getElementById('signInput')
 
 
+document.querySelector('form.signForm').addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  console.log(sign.value);
+var signValue = sign.value
+  fetch("https://octoproxymus.herokuapp.com?secret=walrus&url=http://ohmanda.com/api/horoscope/" + signValue, {
+    'Access-Control-Allow-Origin': '*'
+  })
+      .then(response => response.json())
+      .then(function(astrology) {
+        //console.log(astrology)
+        displayHoroscope(astrology)
+      })
+  
+
+})
+
+function displayHoroscope(data) {
+  document.querySelector('.horoscopeEl').innerHTML= ""
+  console.log(data)
+  var horoscope = document.createElement('p')
+  horoscope.textContent = data.horoscope
+  document.querySelector('.horoscopeEl').append(horoscope)
+}
 
 
-{
-    "day": "25",
-    "month": "12",
-    "year": "1988",
-    "hour": "2",
-    "min": "30",
-    "name": "demo",
-    "lat": "25.123",
-    "lon": "82.34",
-    "tzone": "5.5"
-  }
+// fetch("https://mercuryretrogradeapi.com/?date=" + date, {
+//   'Access-Control-Allow-Origin': '*'
+// })
+//     .then(response => response.json())
+//     .then(function(retrograde) {
+//       console.log(retrograde)
+//     })
+
+// function handleSubmitSign(event) {
+  
+//   console.log(sign)
+// }
