e24520576171928819bde1b6601fda6f424a473b
Brian Bulowicz <bajebi.bb@gmail.com>
2022-10-09 21:48:03 -0500

    4th commit

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-console.log("whats up")
 
 
 
-
-{
-    "day": "25",
-    "month": "12",
-    "year": "1988",
-    "hour": "2",
-    "min": "30",
-    "name": "demo",
-    "lat": "25.123",
-    "lon": "82.34",
-    "tzone": "5.5"
-  }
