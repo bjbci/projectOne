bdd3f3983d45f11da7c2f76f49e57bb0f7adf175
Brian Bulowicz <bajebi.bb@gmail.com>
2022-10-15 03:16:37 -0500

    css finished, daily mercury retrograde working

--- throwthisaway/script.js
+++ throwthisaway/script.js
@@ -1,3 +1,4 @@
+
 var form = document.querySelector('.signForm')
 //var date = dateInput.value
 var signBtn = document.getElementById('signSubmit')
@@ -5,9 +6,6 @@ var signBtn = document.getElementById('signSubmit')
 var mercuryBtn = document.getElementById('mercurySubmit')
 var mercury = document.getElementById('dateInput')
 var dateInput = document.getElementById('dateInput')
-
-
-
 document.querySelector('#signSelector').addEventListener('click', function (e) {
 e.preventDefault();
 console.log(e)
@@ -20,11 +18,8 @@ var signValue = e.target.id
        displayHoroscope(astrology)
       })
 })
-
-
 document.querySelector('form.mercuryForm').addEventListener('submit', function (e) {
   e.preventDefault();
-
   console.log(mercury.value);
 var dateValue = dateInput.value
   fetch("https://mercuryretrogradeapi.com/?date=" + dateValue, {
@@ -34,35 +29,43 @@ var dateValue = dateInput.value
       .then(function(mercury) {
       displayMercury(mercury)
       })
-  
-
 })
 
 function displayHoroscope(data) {
   document.querySelector('.horoscopeEl').innerHTML= ""
   console.log(data)
-  var horoscope = document.createElement('p')
+  var horoscope = document.createElement('p');
   horoscope.textContent = data.horoscope
   document.querySelector('.horoscopeEl').append(horoscope)
 }
 
 function displayMercury(data2) {
-  document.querySelector('.mercuryEl').innerHTML= ""
-  console.log(data2)
-  var mercury = document.createElement('p')
-  mercury.textContent = data2.mercury
-  document.querySelector('.mercuryEl').append(mercury)
+  //console.log(data2)
+  var src =  data2.is_retrograde?"mercuryis.png": "mercurynot2.png"; //ternary operator 
+  document.querySelector('.mercuryEl').innerHTML = `<img src="${src}" alt="Mercury Retrograde Graphic">` //template literals
 }
 
-// fetch("https://mercuryretrogradeapi.com/?date=" + date, {
-//   'Access-Control-Allow-Origin': '*'
-// })
-//     .then(response => response.json())
-//     .then(function(retrograde) {
-//       console.log(retrograde)
-//     })
+var isIsNot=document.getElementById("isIsNotRetrograde")
+console.log(isIsNot)
+fetch("https://mercuryretrogradeapi.com")
+.then(function(response){
+    return response.json()
 
-// function handleSubmitSign(event) {
-  
-//   console.log(sign)
-// }
+})
+.then(function(jsonData){
+    console.log(jsonData)
+    if (jsonData === true){
+        isIsNot.innerText="MERCURY IS IN RETROGRADE TODAY"
+    }else {
+        isIsNot.innerText="TODAY MERCURY IS NOT IN RETROGRADE"
+    }
+})
+console.log(dateInput.multiple)
+console.log(mercury.input)
+var mercuryRetrogradeHx={
+    dateSelected:dateInput.value,
+    textWill:"Mercury will be in retrograde",
+    textWillNot:"Mercury will not be in retrograde",
+}
+localStorage.setItem("mercuryRetrogradeHx",jsonData)
+console.log(isRetrograde)
